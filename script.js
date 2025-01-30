10a658b228a68f0cd744f699329a944bbaa4cfd2
SrieeVathsan <srieevathsanpr@gmail.com>
2025-01-30 14:03:35 +0530

    Initial commit - Portfolio Website

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
-
-    alert(`Thank you, ${name}! Your message has been sent.`);
-});
+const menubar = document.querySelector('#menu');
+const Navbar = document.querySelector('.navbar');
+menubar.onclick=()=>{
+    menubar.classList.toggle('bx-x');
+    Navbar.classList.toggle('active')
+}
+const section=document.querySelectorAll('section');
+const navlink = document.querySelectorAll('header nav a')
+window.onscroll = ()=>{
+    section.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+        if(top>offset && top < offset + height){
+            sec.classList.add('start-animation');
+            navlink.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*='+id+']').classList.add('active')
+              
+            })
+        }
+    })
+    var header = document.querySelector('.header');
+    header.classList.toggle('sticky',window.scrollY>100)
+    menubar.classList.remove('bx-x');
+    Navbar.classList.remove('active')
+} 
 
